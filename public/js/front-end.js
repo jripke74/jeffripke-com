cfdb7aabefd4924c13037fd9a72b60f5e3f1bbaf
jripke74 <jripke74@gmail.com>
2020-02-05 16:06:16 -0800

    add fixed nav, smoth scrolling

--- public/js/front-end.js
+++ public/js/front-end.js
@@ -1,3 +1,16 @@
 'use strict'
 
 document.querySelector('#year').innerHTML = new Date().getFullYear()
+
+// Smooth scrolling
+$('#main-nav a').on('click', function (e) {
+  if (this.hash !== '') {
+    e.preventDefault()
+
+    $('html, body').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 800, () => {
+      window.location.hash = this.hash
+    })
+  }
+})
