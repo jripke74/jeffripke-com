b432677111e05e98a006c2e2f9dbf3ec611e57fe
jripke74 <jripke74@gmail.com>
2020-02-17 17:08:55 -0800

    add background image

--- public/js/front-end.js
+++ public/js/front-end.js
@@ -2,7 +2,7 @@
 
 document.querySelector('#year').innerHTML = new Date().getFullYear()
 
-// Smooth scrolling
+Smooth scrolling
 $('#main-nav a').on('click', function (e) {
   if (this.hash !== '') {
     e.preventDefault()
