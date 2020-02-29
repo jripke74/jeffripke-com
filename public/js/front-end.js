f24021d37caa95dffd0718d8516767c2d7f0bdbc
jripke74 <jripke74@gmail.com>
2020-02-29 12:25:12 -0800

    add about page

--- public/js/front-end.js
+++ public/js/front-end.js
@@ -14,3 +14,12 @@ $('#main-nav a').on('click', function (e) {
     })
   }
 })
+
+$('.port-item').click(function() {
+  $('.collapse').collapse('hide');
+})
+
+$(document).on('click', '[data-toggle="lightbox"]', function (e) {
+  e.preventDefault();
+  $(this).ekkoLightbox();
+})
--- src/app.js
+++ src/app.js
@@ -1,32 +1,39 @@
-'use strict'
+'use strict';
 
-const path = require('path')
-const express = require('express')
-const hbs = require('hbs')
-const port = process.env.PORT || 3000 // on server || locally
+const path = require('path');
+const express = require('express');
+const hbs = require('hbs');
+const port = process.env.PORT || 3000; // on server || locally
 
-const app = express()
+const app = express();
 
 // Define paths for Express config
-const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates/views')
-const partialsPath = path.join(__dirname, '../templates/partials')
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // Setup static directory to serve
-app.set('view engine', 'hbs')
-app.set('views', viewsPath)
-hbs.registerPartials(partialsPath)
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // Setup static directory to serve
-app.use(express.static(publicDirectoryPath))
+app.use(express.static(publicDirectoryPath));
 
 app.get('', (req, res) => {
   res.render('index', {
     name: 'Jeff Ripke',
     pageTitle: 'Home'
-  })
-})
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    name: 'Jeff Ripke',
+    pageTitle: 'About Me'
+  });
+});
 
 app.listen(port, () => {
-  console.log(`server running at port http://localhost:${port}`)
-})
+  console.log(`server running at port http://localhost:${port}`);
+});
